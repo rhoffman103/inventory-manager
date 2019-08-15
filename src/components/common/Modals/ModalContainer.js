995d94f0e733ccd845741c22f2779a12994ee5a1
Bobby H <rhoffman103@gmail.com>
2019-08-15 19:13:12 -0400

    optional modal footer

--- src/components/common/Modals/ModalContainer.js
+++ src/components/common/Modals/ModalContainer.js
@@ -4,10 +4,10 @@ import useModal from '../../../hooks/useModal';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
-const ModalContainer = ({ children, title }) => {
+const ModalContainer = ({ children, title, footer }) => {
     const { state } = useContext(appContext);
     const { closeModal } = useModal();
-
+    
     return (
         <>
             <Modal show={state.isModal} onHide={closeModal}>
@@ -15,11 +15,13 @@ const ModalContainer = ({ children, title }) => {
                     <Modal.Title>{title}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>{children}</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={closeModal}>
-                        Close
-                    </Button>
-                </Modal.Footer>
+                { footer || (typeof footer === 'undefined') &&
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={closeModal}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                }
             </Modal>
         </>
     );
