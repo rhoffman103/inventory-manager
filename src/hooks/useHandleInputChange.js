20ae9cefbbd3b60f8d08ad1bbf3c31be7f40ef42
Bobby H <rhoffman103@gmail.com>
2019-08-15 21:34:48 -0400

    enhanced functionality

--- src/hooks/useHandleInputChange.js
+++ src/hooks/useHandleInputChange.js
@@ -2,11 +2,33 @@ import { useState } from 'react';
 
 const useHandleInputChange = (initial) => {
     const [values, setValue] = useState(initial || {});
+    
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setValue({ ...values, [name]: value });
     };
-    return { values, handleInputChange };
+
+    const emptyValues = (keys) => {
+        let newObj = {}
+        const setNewObj = (arr) => {
+            arr.forEach((key) => {
+                newObj[key] = ''
+            });
+        };
+        
+        if (keys) {
+            if (typeof keys === 'string') {
+                const keysArr = keys.split(' ');
+                setNewObj(keysArr);
+            }
+            else setNewObj(keys);
+        }
+
+        else setNewObj(Object.keys(values));
+    
+        setValue({ ...values, ...newObj });
+    };
+    return { values, handleInputChange, emptyValues };
 };
 
 export default useHandleInputChange;
