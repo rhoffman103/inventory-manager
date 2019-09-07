082727569bf4c968ef87cac2b60906a38c05316f
Bobby H <rhoffman103@gmail.com>
2019-09-06 22:07:24 -0400

    new product form basic collection

---
+++ src/components/adminCompnents/Products/NewProduct/LayerForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import FormControl from 'react-bootstrap/FormControl';
+import MaterialInput from './MaterialInput';
+
+const LayerForm = ({ layer, values, handleInputChange }) => {
+
+    const [materials, setMaterials] = useState([1]);
+    const [pulseClass, setPulse] = useState('');
+    
+    const addMaterials = () => {
+        const newArr = materials.map(num => num);
+        newArr.push(1);
+        setMaterials(newArr);
+    }
+
+    const pulse = () => {
+        setPulse('pulse');
+        const timeoutId = setTimeout(() => {
+            setPulse('');
+            clearTimeout(timeoutId);
+        }, 1250);
+    };
+
+    return (
+        <Row className="border-top new-product-layer">
+            <Col xs={12}>
+                <h5>Layer {layer}</h5>
+            </Col>
+
+            <Col xs={12}>
+                <span>Layer {layer} Percent: </span>
+                <FormControl
+                    className="d-inline col-4 col-md-3"
+                    value={values[`layer${layer}Percent`] || ''}
+                    name={`layer${layer}Percent`}
+                    type="number"
+                    placeholder="30"
+                    onChange={handleInputChange}
+                />
+
+                <div className='my-2 d-flex align-items-center'>
+                    <div
+                        onClick={() => {
+                            pulse();
+                            addMaterials();
+                        }}
+                        className={`${pulseClass} d-inline-flex align-items-center justify-content-center add-icon text-light`}
+                    >
+                        +
+                    </div>
+                    <span className='mx-2'>Add New Material</span>
+                </div>
+
+                <div className='d-flex flex-column-reverse'>
+                    { materials.map((material, index) => {
+                        return (
+                            <MaterialInput
+                                key={`layer${layer}index${index}`}
+                                material={values[`layer${layer}material${index}Name`]}
+                                percentage={values[`layer${layer}material${index}Percentage`]}
+                                index={index}
+                                handleInputChange={handleInputChange}
+                                layer={layer}
+                            />
+                        )
+                    })}
+                </div>
+            </Col>
+
+        </Row>
+    );
+};
+
+export default LayerForm;
--- src/components/adminCompnents/Products/NewProduct/NewProduct.js
+++ src/components/adminCompnents/Products/NewProduct/NewProduct.js
@@ -1,10 +1,119 @@
 import React from 'react';
+import useHandleInputChange from '../../../../hooks/useHandleInputChange';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import FormField from '../../../common/Forms/FormField';
+import Button from 'react-bootstrap/Button';
+import LayerForm from './LayerForm';
+import { collectForm } from '../../../../actions/newProductActions';
 
-const NewProduct = () => {
+const NewProductForm = () => {
+
+    const { values, handleInputChange, emptyValues } = useHandleInputChange({});
+
+    const onSubmit = () => {
+        console.log(collectForm(values));
+    };
+
+    const clearForm = () => {
+        emptyValues();
+    };
 
     return (
-        <div>New product component</div>
+        <div className="p-0 mx-0 mb-4">
+            <Form className="overflow-scroll">
+
+                <Form.Row>
+                    <Col xs={12}>
+                        <FormField
+                            controlId='productId'
+                            value={values.productId || ''}
+                            name='productId'
+                            type='text'
+                            label='Enter Product ID'
+                            placeholder='PR000123'
+                            inputChange={handleInputChange}
+                        />
+                    </Col>
+                    <Col xs={12}>
+                        <FormField
+                            controlId='productDescription'
+                            value={values.productDescription || ''}
+                            name='productDescription'
+                            type='text'
+                            label='Enter Product Description'
+                            placeholder='WH 1.2 PP 38"'
+                            inputChange={handleInputChange}
+                        />
+                    </Col>
+                </Form.Row>
+
+                <Form.Row>
+                    <Col xs={12}><h5>Product Specs</h5></Col>
+                    <Col xs={12} sm={6} m={4}>
+                        <FormField 
+                            controlId="color"
+                            value={values.color || ''}
+                            name="color"
+                            type="text"
+                            label='Color'
+                            placeholder="WH/BlK"
+                            inputChange={handleInputChange}
+                        />
+                    </Col>
+                    <Col xs={12} sm={6} m={4}>
+                        <FormField 
+                            controlId="gauge"
+                            value={values.gauge || ''}
+                            name="gauge"
+                            type="number"
+                            label='Gauge (unit: Mill)'
+                            placeholder="1.2"
+                            inputChange={handleInputChange}
+                        />
+                    </Col>
+                    <Col xs={12} sm={6} m={4}>
+                        <FormField 
+                            controlId="width"
+                            value={values.width || ''}
+                            name="width"
+                            type="number"
+                            label='Width (unit: inches)'
+                            placeholder="38"
+                            inputChange={handleInputChange}
+                        />
+                    </Col>
+                </Form.Row>
+
+                <h4>Formula</h4>
+                <LayerForm layer='A' values={values} handleInputChange={handleInputChange} />
+                <LayerForm layer='B' values={values} handleInputChange={handleInputChange} />
+                <LayerForm layer='C' values={values} handleInputChange={handleInputChange} />
+
+                <Form.Row>
+                    <Col>
+                        <div className="float-right mt-4">
+                            <Button
+                                variant="primary"
+                                type="button"
+                                onClick={onSubmit}
+                            >
+                                Create
+                            </Button>
+
+                            <span 
+                                className="pointer align-middle ml-2"
+                                onClick={clearForm}
+                            >
+                                <u>cancel</u>
+                            </span>
+                        </div>
+                    </Col>
+                </Form.Row>                 
+
+            </Form>
+        </div>
     );
 };
 
-export default NewProduct;
+export default NewProductForm;
