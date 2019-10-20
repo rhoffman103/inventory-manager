3ae26b09d32d6cb741aa4e4e2c11148cbace1da4
Bobby H <rhoffman103@gmail.com>
2019-10-19 23:39:55 -0400

    production nav

--- src/components/common/Nav/ProductionNav.js
+++ src/components/common/Nav/ProductionNav.js
@@ -1,13 +1,19 @@
 import React, { useContext } from 'react';
 import appContext from '../../../context/appContext';
+import { Link } from 'react-router-dom';
 import LogOut from './links/LogOut';
 
 const ProductionNav = () => {
     const { state } = useContext(appContext);
+    const { navbar } = state;
 
     return (
         state.auth.email
-        ?   <LogOut />
+        ?   <>
+                { navbar.page !== 'home' && <Link to="/" className="nav-link">Home</Link> }
+                { navbar.page !== 'production' && <Link to="/production" className="nav-link">Production</Link>}
+                <LogOut />
+            </>
         :   <></>
     );
 };
