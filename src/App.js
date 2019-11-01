d1c0bd87d932335c7e161f45795350788d84da26
Bobby H <rhoffman103@gmail.com>
2019-11-01 15:01:22 -0400

    cleaned up comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router /*, Route, Switch*/ } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import StateProvider from './components/contextProvider/StateProvider';
 import ScrollToTop from './components/ScrollToTop';
 import AppRouter from './routers/AppRouter';
-// import Home from './pages/Home';
-// import Admin from './pages/Admin';
-// import Production from './pages/Production';
 import './css/App.css';
 import './css/Color.css';
 import './css/Animations.css';
@@ -16,11 +13,6 @@ const App = () => (
 		<ScrollToTop>
 			<StateProvider>
 				<AppRouter />
-				{/* <Switch>
-					<Route exact path="/" component={Home} />
-					<Route path="/admin" component={Admin} />
-					<Route path="/production" component={Production} />
-				</Switch> */}
 			</StateProvider>
 		</ScrollToTop>
 	</Router>
