e97addd1137a40d33ba48334969eaffc586e6a6b
Bobby H <rhoffman103@gmail.com>
2019-11-01 11:59:28 -0400

    separating components

--- src/components/productionComponents/AddFinishedProduct/AddFinishedProduct.js
+++ src/components/productionComponents/AddFinishedProduct/AddFinishedProduct.js
@@ -1,46 +1,21 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import appContext from '../../../context/appContext';
-import FormControl from 'react-bootstrap/FormControl';
+import SelectJobJacket from './SelectJobJacket';
 
 const AddFinishedProduct = ({ line }) => {
-    const { state, stateDispatch } = useContext(appContext);
-    const { schedule, selectedJobJacket } = state.db;
-
-    const setSelectJobJacket = (jobJacket) => {
-        stateDispatch({
-            type: 'SELECT_JOB_JACKET',
-            jobJacket
-        });
-    };
-
-    useEffect(() => {
-        schedule && setSelectJobJacket(schedule[0]);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [schedule]);
+    const { state } = useContext(appContext);
+    const { selectedJobJacket } = state.db;
 
     return (
         <>
             <h1>{line}: Add New Rolls</h1>
             <div>
                 { selectedJobJacket &&
-                    <h5>Select Job Jacket: {`${selectedJobJacket.customer}: ${selectedJobJacket.id}`}</h5>
+                    <h5>
+                        Current Job Jacket: {`${selectedJobJacket.customer} - ${selectedJobJacket.id}`}
+                    </h5>
                 }
-                { schedule &&
-                    <FormControl
-                        as="select"
-                        className='br-sharp'
-                        onChange={(e) => setSelectJobJacket(schedule[e.target.value])}
-                    >
-                        { schedule.map((jobJacket, index) => (
-                            <option
-                                key={jobJacket.jobJacketKey}
-                                value={index}
-                            >
-                                {jobJacket.customer}: {jobJacket.id}
-                            </option>
-                        ))}
-                    </FormControl>
-                }
+                <SelectJobJacket />
             </div>
         </>
     );
