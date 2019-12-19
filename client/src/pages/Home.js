ef8087d34b9005fbe33bf579d77e3b43f934f417
Bobby H <rhoffman103@gmail.com>
2019-12-19 08:17:37 -0500

    demo info on homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,7 +13,27 @@ const Home = () => {
             {state.auth.onFirebaseAuth
             ?
                 <div className="container">
-                    <h1>Home Page</h1>
+                    <h2>For Demo Usage</h2>
+                    <p className='mb-1'>Login as a manager.</p>
+                    <ul>
+                        <li>Email: manager@123.com</li>
+                        <li>Password: pass12</li>
+                    </ul>
+                    <p className='mb-1'>Login as an operator.</p>
+                    <ul>
+                        <li>Email: operator@123.com</li>
+                        <li>Password: pass12</li>
+                    </ul>
+
+                    <p>
+                        Inventory Manager is a mock-up application to manage production for a specialized manufacturing process.
+                        Managers can add new products to the database to create new job jackets and update the running schedule.
+                        Operators get an updated schedule to add new product, scrap and downtime while keeping track of a job jackets progress.
+                    </p>
+
+                    <p>
+                        Checkout the source code! <a href="https://github.com/rhoffman103/inventory-manager">https://github.com/rhoffman103/inventory-manager</a>
+                    </p>
                 </div>
             :   <></>
             }
